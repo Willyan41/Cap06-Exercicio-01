5d46eb19ca4e819776539e4051be3a2a5ab09c82
Willyan Santos <willyan41@hotmail.com>
2020-03-31 17:25:56 -0300

    Acrescentado enunciado.

--- Cap06 Exercicio 01/Program.cs
+++ Cap06 Exercicio 01/Program.cs
@@ -6,6 +6,21 @@ namespace Cap06_Exercicio_01
     {
         static void Main(string[] args)
         {
+            /*
+             A dona de um pensionato possui dez quartos para alugar para estudantes,
+                sendo esses quartos identificados pelos números 0 a 9.
+                Quando um estudante deseja alugar um quarto, deve-se registrar o nome
+                e email deste estudante.
+                Fazer um programa que inicie com todos os dez quartos vazios, e depois
+                leia uma quantidade N representando o número de estudantes que vão
+                alugar quartos (N pode ser de 1 a 10). Em seguida, registre o aluguel dos
+                N estudantes. Para cada registro de aluguel, informar o nome e email do
+                estudante, bem como qual dos quartos ele escolheu (de 0 a 9). Suponha
+                que seja escolhido um quarto vago. Ao final, seu programa deve imprimir
+                um relatório de todas ocupações do pensionato, por ordem de quarto,
+                conforme exemplo.
+             */
+
             int nMaxQuarto = 10;
 
             Console.Write("Informe quantos quartos gostaria de reservar? ");
@@ -16,7 +31,7 @@ namespace Cap06_Exercicio_01
 
             for (int i = 0; i < quarto; i++)
             {
-                Console.WriteLine("Reserva #{0}:", i+1);
+                Console.WriteLine("Reserva #{0}:", i + 1);
                 Console.Write("Nome: ");
                 string nome = Console.ReadLine();
                 Console.Write("Email: ");
@@ -32,9 +47,8 @@ namespace Cap06_Exercicio_01
             Console.WriteLine("Quartos Reservados");
             for (int i = 0; i < 10; i++)
             {
-                if (vect[i]!= null)
+                if (vect[i] != null)
                     Console.WriteLine(vect[i]);
-
             }
 
 
