fa46e9e596cd1453aed6dd71866a39a6cdb7f070
Willyan Santos <willyan41@hotmail.com>
2020-03-31 17:20:53 -0300

    Adicionar arquivos de projeto.

---
+++ Cap06 Exercicio 01/Estudante.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cap06_Exercicio_01
+{
+    class Estudante
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Quarto { get; set; }
+
+        public Estudante(string nome, string email, int quarto)
+        {
+            Nome = nome;
+            Email = email;
+            Quarto = quarto;
+        }
+
+        public override string ToString()
+        {
+            return
+                Quarto +
+                ": " +
+                Nome +
+                ", " +
+                Email;
+        }
+
+
+    }
+}
---
+++ Cap06 Exercicio 01/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Cap06_Exercicio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nMaxQuarto = 10;
+
+            Console.Write("Informe quantos quartos gostaria de reservar? ");
+            int quarto = int.Parse(Console.ReadLine());
+
+
+            Estudante[] vect = new Estudante[nMaxQuarto];
+
+            for (int i = 0; i < quarto; i++)
+            {
+                Console.WriteLine("Reserva #{0}:", i+1);
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quart = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                vect[quart] = new Estudante(nome, email, quart);
+
+            }
+
+            Console.WriteLine("Quartos Reservados");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i]!= null)
+                    Console.WriteLine(vect[i]);
+
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
